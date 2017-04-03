9d507a8d2d0d9afeef668d03a2e84d1c1a1b67a3
Pablo Sanchez <psanchez@xpreso.com>
2017-04-03 16:53:35 +0100

    [ADD] - User authentication persisted with cookies

--- cr-acl.js
+++ cr-acl.js
@@ -1,157 +1,180 @@
-angular.module("cr.acl", [])
-.constant("cr-acl.config", {
-  "redirect": "unauthorized",
-  "roles": {
-    "ROLE_USER": ["ROLE_USER"],
-    "ROLE_GUEST": ["ROLE_GUEST"]
-  }
-})
-.provider("crAcl", ['cr-acl.config', function(config){
-  var self = {};
-  self.roles = config.roles;
-  self.redirect = config.redirect;
-
-  /**
-   * Your role is granted for this route?
-   * @param string identityRole
-   * @param array  sstateRolesGranted
-   * @return bool
-   */
-  self.isGranted = function(identityRole, stateRolesGranted){
-    var granted = false;
-
-    if((identityRole in self.roles) === false){
-      throw "This role["+identityRole+"] not exist into InheritanceRoles declaration";
+angular.module("cr.acl", ['ngCookies']).constant("cr-acl.config", {
+    "redirect": "unauthorized",
+    "roles": {
+        "ROLE_USER": ["ROLE_USER"],
+        "ROLE_GUEST": ["ROLE_GUEST"]
     }
-
-    if(stateRolesGranted.indexOf(identityRole) !== -1){
-      granted = true;
-    }
-
-    for(var ii in self.roles[identityRole]) {
-      if(stateRolesGranted.indexOf(self.roles[identityRole][ii]) !== -1){
-        granted = true;
-      }
-    }
-
-    return granted;
-  };
-
-  this.$get = ['$q', '$rootScope', '$injector', function($q, $rootScope, $injector){
-    var crAcl = {};
-
+}).provider("crAcl", ['cr-acl.config', function(config) {
+    var self = {};
+    self.roles = config.roles;
+    self.redirect = config.redirect;
     /**
-     * Configure roles tree
-     * @param arrat roles
+     * Your role is granted for this route?
+     * @param string identityRole
+     * @param array  sstateRolesGranted
+     * @return bool
      */
-    crAcl.setInheritanceRoles = function(roles){
-        angular.forEach(roles, function(inheritance, roleName){
-        if(roleName == "ROLE_USER" && roleName == "ROLE_GUEST"){
-            throw roleName+" is a reserved world because is a father of ROLE, you can not override it";
+    self.isGranted = function(identityRole, stateRolesGranted) {
+        var granted = false;
+        if ((identityRole in self.roles) === false) {
+            throw "This role[" + identityRole + "] not exist into InheritanceRoles declaration";
         }
-        self.roles[roleName] = inheritance;
-        });
-    };
-
-    /**
-     * Set route name for redirect after unauthorized operation
-     */
-    crAcl.setRedirect = function(redirectStateName){
-        self.redirect = redirectStateName;
-    };
-
-    /**
-     * Set Role
-     * @param string role
-     */
-    crAcl.setRole = function(role){
-        self.identityRole = role;
-    };
-
-    /**
-    * Return your role
-    * @return string
-    */
-    crAcl.getRole = function(){
-        if(self.identityRole === undefined){
-          return "ROLE_GUEST";
+        if (stateRolesGranted.indexOf(identityRole) !== -1) {
+            granted = true;
         }
-        return self.identityRole;
-    };
-
-    var afterChangeStart = function(event, toState, toParams, fromState, fromParams) {
-      if(!toState.data || !toState.data.is_granted){
-        return crAcl;
-      }
-      if(toState.data.is_granted[0] === "*"){
-        return crAcl;
-      }
-      var is_allowed = (toState.data.is_granted !== undefined) ? toState.data.is_granted : ["ROLE_GUEST"];
-      var isGranted = self.isGranted(crAcl.getRole(), is_allowed);
-      return isGranted;
-    };
-
-    $rootScope.$on('$stateChangeStart',  function(event, toState, toParams, fromState, fromParams) {
-        $injector.invoke(["$state", function($state) {
-          var isGranted = afterChangeStart(event, toState, toParams, fromState, fromParams);
-
-          if(!isGranted && self.redirect !== false){
-            event.preventDefault();
-            if(self.redirect != toState.name) {
-              $state.go(self.redirect);
+        for (var ii in self.roles[identityRole]) {
+            if (stateRolesGranted.indexOf(self.roles[identityRole][ii]) !== -1) {
+                granted = true;
             }
-          }
-        }]);
-    });
-
-    $rootScope.$on('$routeChangeStart',  function(event, toState, toParams, fromState, fromParams) {
-        $injector.invoke(["$location", function($location) {
-          var isGranted = afterChangeStart(event, toState, toParams, fromState, fromParams);
-
-          if(!isGranted && self.redirect !== false){
-            event.preventDefault();
-            if(self.redirect != toState.name) {
-              $location.path(self.redirect);
-            }
-          }
-        }]);
-    });
-
-    return crAcl;
-
-  }];
-}])
-.directive("crGranted", ['crAcl', '$animate', function(acl, $animate){
-  return {
-    restrict: "A",
-    replace: false,
-    transclude: 'element',
-    terminal: true,
-    link: function(scope, elem, attr, ctrl, $transclude){
-      var content = false;
-      $transclude(function(clone, newScope) {
-        childScope = newScope;
-        clone[clone.length++] = document.createComment(' end crGranted: ' + attr.crGranted + ' ');
-        block = {
-          clone: clone
-        };
-        content = clone;
-      });
-
-
-      scope.$watch(function() {
-        return acl.getRole();
-      }, function(newV, oldV){
-      var allowedRoles = attr.crGranted.split(",");
-      if(allowedRoles.indexOf(acl.getRole()) != -1) {
-        $animate.enter(content, elem.parent(), elem);
-      }
-      else {
-        if(content) {
-          content.remove();
         }
-      }
-      });
-    }
-  };
+        return granted;
+    };
+    this.$get = ['$q', '$rootScope', '$injector', '$cookies', '$state',
+        function($q, $rootScope, $injector, $cookies, $state) {
+            var crAcl = {};
+            /**
+             * Configure roles tree
+             * @param arrat roles
+             */
+            crAcl.setInheritanceRoles = function(roles) {
+                angular.forEach(roles, function(inheritance, roleName) {
+                    if (roleName == "ROLE_USER" && roleName == "ROLE_GUEST") {
+                        throw roleName + " is a reserved world because is a father of ROLE, you can not override it";
+                    }
+                    self.roles[roleName] = inheritance;
+                });
+            };
+            /**
+             * Set route name for redirect after unauthorized operation
+             */
+            crAcl.setRedirect = function(redirectStateName) {
+                self.redirect = redirectStateName;
+            };
+            /**
+             * Unset Role
+             * @param string role
+             */
+            crAcl.unsetRole = function(role) {
+                $cookies.remove('identityRole');
+            };
+            /**
+             * Set Role
+             * @param string role
+             */
+            crAcl.setRole = function(role) {
+                $cookies.put('identityRole', role);
+            };
+            /**
+             * Get Redirect From Params
+             * @param string role
+             */
+            crAcl.getFromParams = function(role) {
+                var from = $cookies.getObject('fromParams');
+                return from;
+            };
+            /**
+             * Get Redirect From State
+             * @param string role
+             */
+            crAcl.getFromState = function(role) {
+                return $cookies.getObject('fromState');
+            };
+            crAcl.resetFromState = function(role) {
+                $cookies.remove('fromState');
+                $cookies.remove('fromParams');
+            };
+            /**
+             * Return your role
+             * @return string
+             */
+            crAcl.getRole = function() {
+                if ($cookies.get('identityRole') === undefined) {
+                    return "ROLE_GUEST";
+                }
+                return $cookies.get('identityRole');
+            };
+            var afterChangeStart = function(event, toState, toParams, fromState, fromParams) {
+                if (!toState.data || !toState.data.is_granted) {
+                    return crAcl;
+                }
+                if (toState.data.is_granted[0] === "*") {
+                    return crAcl;
+                }
+                var is_allowed = (toState.data.is_granted !== undefined) ? toState.data.is_granted : ["ROLE_GUEST"];
+                var isGranted = self.isGranted(crAcl.getRole(), is_allowed);
+                return isGranted;
+            };
+            /**
+             *  getNestedStateUrl
+             */
+            $rootScope.getNestedStateUrl = function(state) {
+                var url = state.url;
+                if (state.parent) {
+                    var parentState = $state.get(state.parent);
+                    url = $rootScope.getNestedStateUrl(parentState) + url;
+                }
+                return url;
+            }
+            $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+                $injector.invoke(
+                    ["$state", "$location", "$urlMatcherFactory",
+                        function($state, $location, $urlMatcherFactory) {
+                            var isGranted = afterChangeStart(event, toState, toParams, fromState, fromParams);
+                            var search = $location.search();
+                            var path = $location.path();
+                            var stateParams;
+                            if (!isGranted && self.redirect !== false) {
+                                event.preventDefault();
+                                if ($location.path() == "/" && !$location.search()) {
+                                    crAcl.resetFromState();
+                                } else {
+                                    angular.forEach($state.get(), function(state) {
+                                        var nestedStateUrl = $rootScope.getNestedStateUrl(state);
+                                        var urlMatcher = $urlMatcherFactory.compile(nestedStateUrl);
+                                        if (stateParams = urlMatcher.exec(path, search)) {
+                                            $cookies.putObject('fromState', state);
+                                            $cookies.putObject('fromParams', stateParams);
+                                        }
+                                    })
+                                }
+                                if (self.redirect != toState.name) {
+                                    $state.go(self.redirect);
+                                }
+                            }
+                        }
+                    ]);
+            });
+            return crAcl;
+        }
+    ];
+}]).directive("crGranted", ['crAcl', '$animate', function(acl, $animate) {
+    return {
+        restrict: "A",
+        replace: false,
+        transclude: 'element',
+        terminal: true,
+        link: function(scope, elem, attr, ctrl, $transclude) {
+            var content = false;
+            $transclude(function(clone, newScope) {
+                childScope = newScope;
+                clone[clone.length++] = document.createComment(' end crGranted: ' + attr.crGranted + ' ');
+                block = {
+                    clone: clone
+                };
+                content = clone;
+            });
+            scope.$watch(function() {
+                return acl.getRole();
+            }, function(newV, oldV) {
+                var allowedRoles = attr.crGranted.split(",");
+                if (allowedRoles.indexOf(acl.getRole()) != -1) {
+                    $animate.enter(content, elem.parent(), elem);
+                } else {
+                    if (content) {
+                        content.remove();
+                    }
+                }
+            });
+        }
+    };
 }]);
